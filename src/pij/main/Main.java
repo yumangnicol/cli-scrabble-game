3498062633877b649e7eac9272825fe2ac43f452
nicolluisyumang <yumangnicol@gmail.com>
2022-12-07 19:51:41 +0000

    Add ScrabbleBoard.java class with private fields size and board

--- src/pij/main/Main.java
+++ src/pij/main/Main.java
@@ -1,5 +1,9 @@
 package pij.main;
 
 public class Main {
-
+    public static void main(String[] args) {
+        ScrabbleBoard sb = new ScrabbleBoard();
+        sb.print();
+    }
 }
+
---
+++ src/pij/main/ScrabbleBoard.java
@@ -0,0 +1,14 @@
+package pij.main;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class ScrabbleBoard {
+
+    private int size;
+    private String[][] board;
+
+
+}
+
