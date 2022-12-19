add06ee882c5c2f02572f5c12d35b1a891acf443
nicolluisyumang <yumangnicol@gmail.com>
2022-12-19 17:21:26 +0000

    Add refill(LetterBag, int) method in LetterRack.java which draws a specified amount of letters from the bag to the rack

--- src/pij/main/LetterRack.java
+++ src/pij/main/LetterRack.java
@@ -3,12 +3,14 @@ package pij.main;
 import java.util.ArrayList;
 
 public class LetterRack {
-    ArrayList<Letter> rack = new ArrayList<>();
+    ArrayList<Letter> rack;
     final private int MAX_RACK_SIZE = 7;
 
     public LetterRack(LetterBag letterBag){
-//        letterBag.
+        this.rack = letterBag.drawLetters(this.MAX_RACK_SIZE);
     }
 
-
+    public void refill(LetterBag letterBag, int count){
+        this.rack.addAll(letterBag.drawLetters(count));
+    }
 }
--- src/pij/main/Player.java
+++ src/pij/main/Player.java
@@ -2,7 +2,6 @@ package pij.main;
 
 public class Player {
     int score;
-
     public Player() {
         this.score = 0;
     }
