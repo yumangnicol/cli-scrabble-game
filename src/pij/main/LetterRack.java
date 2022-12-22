a69a226f5feff03f2871a844b0e88511a8fe2e39
nicolluisyumang <yumangnicol@gmail.com>
2022-12-22 22:31:29 +0000

    Refactor refill(LetterBag, int) method to handle invalid count inputs

--- src/pij/main/LetterRack.java
+++ src/pij/main/LetterRack.java
@@ -3,14 +3,22 @@ package pij.main;
 import java.util.ArrayList;
 
 public class LetterRack {
-    ArrayList<Letter> rack;
+    private ArrayList<Letter> rack;
     final private int MAX_RACK_SIZE = 7;
 
-    public LetterRack(LetterBag letterBag){
-        this.rack = letterBag.drawLetters(this.MAX_RACK_SIZE);
+    public LetterRack(){
+        this.rack = new ArrayList<>();
     }
 
-    public void refill(LetterBag letterBag, int count){
+    public boolean refill(LetterBag letterBag, int count){
+        if(count == 0 || count > this.getRackSpace()) {
+            return false;
+        }
         this.rack.addAll(letterBag.drawLetters(count));
+        return true;
+    }
+
+    private int getRackSpace() {
+        return this.MAX_RACK_SIZE - this.rack.size();
     }
 }
--- src/pij/main/Player.java
+++ src/pij/main/Player.java
@@ -2,7 +2,24 @@ package pij.main;
 
 public class Player {
     int score;
+    LetterRack rack;
+
     public Player() {
         this.score = 0;
+    }
+
+    public Player(LetterBag letterBag){
+        this.score = 0;
+        this.rack = new LetterRack();
+    }
+
+    public void makeMove(String move){
+        String[] moveStr = move.split(",");
+
+        //0 Letters to use
+
+        // Position
+
+        // Down or Right
     }
 }
