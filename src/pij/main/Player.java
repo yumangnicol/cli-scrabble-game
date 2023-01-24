b6a30ddc52dadab2d32fe1b1731228ededcf7a32
nicolluisyumang <yumangnicol@gmail.com>
2023-01-24 23:28:29 +0000

    Add getter and setter for score field of Player.java

--- src/pij/main/Player.java
+++ src/pij/main/Player.java
@@ -13,28 +13,11 @@ public class Player {
         this.rack = new LetterRack();
     }
 
-    public boolean makeMove(String move, ScrabbleBoard board){
-        String[] moveStr = move.split(",");
-        // ADD VALIDATION to move string ❗️
+    public int getScore() {
+        return score;
+    }
 
-        //0 Letters to use
-        char[] letters = moveStr[0].toCharArray();
-        if (!this.rack.containsAll(letters)){
-            return false;
-        }
-
-        // Position
-        final int ASCII_SUBTRACTOR = 96; // 96 because the decimal value of char a starts at 97
-        String position = moveStr[1];
-        int row = Integer.parseInt(position.substring(1));
-        int col = position.charAt(0) - ASCII_SUBTRACTOR ;
-        if (!board.isValidMove(row, col)){
-            return false;
-        }
-
-        // Down or Right
-
-        // Check Dictionary
-        return true;
+    public void addScore(int points) {
+        this.score += points;
     }
 }
