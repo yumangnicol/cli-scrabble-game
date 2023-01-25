818deb42363ad0e5e9dea7e2d34a4e8610f7c099
nicolluisyumang <yumangnicol@gmail.com>
2023-01-25 19:27:14 +0000

    Add Move.java class and MoveTest.java for the test cases

---
+++ src/pij/main/Move.java
@@ -0,0 +1,40 @@
+package pij.main;
+
+import java.util.Arrays;
+
+public class Move {
+    private char[] letters;
+    private int row;
+    private int col;
+    private boolean right;
+
+    public Move(String moveString) {
+        final int ASCII_SUBTRAHEND = 96;
+        String[] move = moveString.split(",");
+
+        if(move.length != 3){
+            throw new IllegalArgumentException("Move not accepted! Invalid string format");
+        }
+
+        if(!move[0].matches("^[a-zA-Z]*$")) {
+            throw new IllegalArgumentException("Move not accepted! Played word should not contain special characters");
+        }
+        this.letters = move[0].toCharArray();
+
+        if(!Character.isLowerCase(move[1].charAt(0))){
+            throw new IllegalArgumentException("Move not accepted! Column should be in lower-case");
+        }
+        int col = move[1].charAt(0) - ASCII_SUBTRAHEND;
+
+        try{
+            this.row = Integer.parseInt(move[1].substring(1));
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("Move not accepted! Row should be in correct int format");
+        }
+
+        if(move[2].charAt(0) != 'r' || move[2].charAt(0) == 'd'){
+            throw new IllegalArgumentException("Move not accepted! Direction should be in either r - right or d - down only");
+        }
+        this.right = move[2].charAt(0) == 'r';
+    }
+}
---
+++ tests/MoveTest.java
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.Test;
+import pij.main.Move;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MoveTest {
+    @Test
+    public void testMoveInvalidFormat(){
+        String move = "GIT,f8,r,asd";
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Move(move));
+        String expectedMessage = "Move not accepted! Invalid string format";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(actualMessage, expectedMessage);
+    }
+
+    @Test
+    public void testMoveSpecialCharactersOnPlayedWord(){
+        String move = "!GIT,f8,r";
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Move(move));
+        String expectedMessage = "Move not accepted! Played word should not contain special characters";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(actualMessage, expectedMessage);
+    }
+
+    @Test
+    public void testMoveColumnUpperCase(){
+        String move = "GIT,F8,r";
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Move(move));
+        String expectedMessage = "Move not accepted! Column should be in lower-case";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(actualMessage, expectedMessage);
+    }
+
+    @Test
+    public void testMoveRowNotInt(){
+        String move = "GIT,fD8,r";
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Move(move));
+        String expectedMessage = "Move not accepted! Row should be in correct int format";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(actualMessage, expectedMessage);
+    }
+
+    @Test
+    public void testMoveInvalidDirection(){
+        String move = "GIT,f8,R";
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Move(move));
+        String expectedMessage = "Move not accepted! Direction should be in either r - right or d - down only";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(actualMessage, expectedMessage);
+    }
+}
