f537e272d0b7a81c162ff1bdd42f018a9eca91a7
nicolluisyumang <yumangnicol@gmail.com>
2023-02-01 19:44:17 +0000

    Add logic to handle drawing Letters from the LetterBag when the bag is empty

--- src/pij/main/LetterBag.java
+++ src/pij/main/LetterBag.java
@@ -17,6 +17,7 @@ public class LetterBag {
         for(int i = 0; i < Alphabet.getLetterCount().get(' '); i++){
             letters.add(new Letter(' '));
         }
+        this.shuffle();
     }
     public void shuffle() {
         Collections.shuffle(this.letters);
@@ -32,6 +33,13 @@ public class LetterBag {
 
     public ArrayList<Letter> drawLetters(int count){
         ArrayList<Letter> drawnLetters = new ArrayList<>();
+
+        // Returns empty if bag is empty
+        if(this.letters.size() == 0){
+            return drawnLetters;
+        }
+
+        // Create logic here if count > letters.count
 
         for(int i = 0; i < count; i++){
             int rando = (int) (Math.random() * (this.size()-1)); // Generates random number from 0 to Length
