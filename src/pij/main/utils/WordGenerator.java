09ae65485bd57c00d2d3f72ff373bdb53900cdb5
nicolluisyumang <yumangnicol@gmail.com>
2023-02-23 17:46:21 +0000

    refactor(WordGenerator): generateWords() method returns ArrayList<String>

--- src/pij/main/utils/WordGenerator.java
+++ src/pij/main/utils/WordGenerator.java
@@ -6,20 +6,23 @@ import pij.main.WordList;
 import java.util.ArrayList;
 
 public class WordGenerator {
-    public static void generateWords(ArrayList<Tile> tiles, int length, String start, WordList wordList) {
-        generateWordsHelper(start, tiles, length, wordList);
+    public static ArrayList<String> generateWords(ArrayList<Tile> tiles, int maxLength, String start, WordList wordList) {
+        ArrayList<String> words = new ArrayList<>();
+        for (int length = 1; length <= maxLength; length++) {
+            generateWordsHelper(start, tiles, length, wordList, words);
+        }
+        return words;
     }
 
-    // Limitation: Does not utilize wildcards
-    private static void generateWordsHelper(String currentWord, ArrayList<Tile> tiles, int length, WordList wordlist) {
-        if (currentWord.length() == length) {
-            if(wordlist.contains(currentWord.toLowerCase())){
-                System.out.println(currentWord);
-            }
+    private static void generateWordsHelper(String currentWord, ArrayList<Tile> tiles, int maxLength, WordList wordList, ArrayList<String> words) {
+        if (currentWord.length() > maxLength) {
             return;
         }
+        if (wordList.contains(currentWord.toLowerCase()) && !words.contains(currentWord)) {
+            words.add(currentWord);
+        }
         for (Tile tile : tiles) {
-            generateWordsHelper(currentWord + tile.getLetter(), tiles, length, wordlist);
+            generateWordsHelper(currentWord + tile.getLetter(), tiles, maxLength, wordList, words);
         }
     }
 }
