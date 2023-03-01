b3a739cbd4045958a0aa8c086fc9ceb8d30a4598
nicolluisyumang <yumangnicol@gmail.com>
2023-03-01 20:38:23 +0000

    refactor(WordGenerator): Change maxLength to 8

--- src/pij/main/utils/WordGenerator.java
+++ src/pij/main/utils/WordGenerator.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class WordGenerator {
     public static ArrayList<String> generateWords(ArrayList<Tile> tiles, int maxLength, String start, WordList wordList) {
         ArrayList<String> words = new ArrayList<>();
-        maxLength = Math.min(maxLength, 7);
+        maxLength = Math.min(maxLength, 8);
         for (int length = 1; length <= maxLength; length++) {
             generateWordsHelper(start, tiles, length, wordList, words);
         }
