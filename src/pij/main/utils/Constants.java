9206b360f2a01dab4c4df84e238874dc731c8532
nicolluisyumang <yumangnicol@gmail.com>
2023-03-02 22:38:26 +0000

    refactor(Constants): change regex for premium square format

--- src/pij/main/utils/Constants.java
+++ src/pij/main/utils/Constants.java
@@ -3,7 +3,7 @@ package pij.main.utils;
 public class Constants {
     public static final String DEFAULT_BOARD_FILE = "./resources/defaultBoard.txt";
     public static final String DEFAULT_WORD_LIST_FILE = "./resources/wordlist.txt";
-    public static final String PREMIUM_SQUARE_REGEX_FORMAT = "\\([0-9]{2}|\\{[0-9]{2}|\\([0-9]\\)|\\{[0-9]\\}";
+    public static final String PREMIUM_SQUARE_REGEX_FORMAT = "\\(\\-[1-9]|\\{\\-[1-9]|\\([0-9]{2}|\\{[0-9]{2}|\\([0-9]\\)|\\{[0-9]\\}";
     public static final String MOVE_STRING_REGEX_FORMAT = "^[a-zA-Z]*$";
     public static final int MIN_BOARD_SIZE = 12;
     public static final int MAX_BOARD_SIZE = 26;
