90e6cfc9b5ab463af56517f41ad63878918fb7d5
nicolluisyumang <yumangnicol@gmail.com>
2023-03-07 19:38:01 +0000

    Fix(WordGenerator): add handler when starting letter provided is blank

--- src/pij/main/utils/WordGenerator.java
+++ src/pij/main/utils/WordGenerator.java
@@ -5,16 +5,20 @@ import pij.main.WordList;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 
 public class WordGenerator {
-    public static List<String> generateWords(ArrayList<Tile> tiles, int maxLength, String start, WordList wordList) {
+    public static ArrayList<String> generateWords(ArrayList<Tile> tiles, int maxLength, String start, WordList wordList) {
         ArrayList<String> words = new ArrayList<>();
         maxLength = Math.min(maxLength, 8);
         HashSet<Tile> usedTiles = new HashSet<>();
-        generateWordsHelper(start, tiles, maxLength, wordList, words, usedTiles);
-        
-        return words.stream().filter(s -> !s.equals("")).toList();
+        if (start.isEmpty()) {
+            for (Tile tile : tiles) {
+                generateWordsHelper(String.valueOf(tile.getLetter()), tiles, maxLength, wordList, words, usedTiles);
+            }
+        } else {
+            generateWordsHelper(start, tiles, maxLength, wordList, words, usedTiles);
+        }
+        return words;
     }
 
     private static void generateWordsHelper(String currentWord, ArrayList<Tile> tiles, int maxLength, WordList wordList, ArrayList<String> words, HashSet<Tile> usedTiles) {
