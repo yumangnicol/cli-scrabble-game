728b1ca3d4c5d9bebea2c5a0706c06f510ff3644
nicolluisyumang <yumangnicol@gmail.com>
2023-03-14 21:57:56 +0000

    docs(MoveGenerator): Add java docs to MoveGenerator class

--- src/pij/main/MoveGenerator.java
+++ src/pij/main/MoveGenerator.java
@@ -6,8 +6,89 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * A generator that creates a move to play
+ * @author Nicol Luis Yumang
+ */
 public class MoveGenerator {
 
+    /**
+     * Creates a new move from a constructed move string using the given parameters
+     * @param word the word of the move
+     * @param row the starting row of the move
+     * @param col the starting column of the move
+     * @param goingRight the direction of the move
+     * @param firstMove true if the constructed move is the first move of the game, false otherwise
+     * @return new move to be played
+     */
+    private static Move constructMove(String word, int row, int col, boolean goingRight, boolean firstMove){
+
+        StringBuilder sb = new StringBuilder();
+        String moveStr = firstMove ? word : word.substring(1);
+        sb.append(moveStr);
+        sb.append(',');
+        char rowLetter = (char) (col + Constants.CHAR_INT_VALUE_BEFORE_SMALL_LETTER_A);
+        sb.append(rowLetter);
+        sb.append(row);
+        sb.append(',');
+        char direction = goingRight ? 'r' : 'd';
+        sb.append(direction);
+
+        return new Move(sb.toString());
+    }
+
+    /**
+     * Counts the number of available space for a move going down
+     * @param board the board the move is played
+     * @param row the starting row of the move
+     * @param col the starting column of the move
+     * @return number of available space going down
+     */
+    private static int downMoveAvailableSpace(ScrabbleBoard board, int row, int col){
+        int currRow = row;
+        int space = 0;
+        // Check: top of connecting square is not out of bounds and empty
+        if((currRow - 1) > 1 && !board.isSquareEmpty(currRow - 1, col)){
+            return 0;
+        }
+        // Check: available space downward
+        while(currRow < board.length()
+                && board.isSquareEmpty(currRow + 1, col)
+                && !board.isSquareHorizontalAdjacent(currRow + 1, col)){
+            space++;
+            currRow++;
+        }
+        return space;
+    }
+
+    /**
+     * Creates a move to be played as a first move of the game given the available tiles in the rack
+     * @param rack the list of tiles to use for the move
+     * @param board the board the move is played
+     * @param wordList the list of words to check if move is valid
+     * @return valid move to play as a first move of the game
+     */
+    public static Move generateFirstMove(TileRack rack, ScrabbleBoard board, WordList wordList){
+        List<String> validMoves = WordGenerator.generateWords(rack.getTiles(), 7, "", wordList);
+        Optional<String> optionalMove = validMoves.stream().max(Comparator.comparing(String::length));
+        if(validMoves.size() > 0){
+            String finalMove = optionalMove.get();
+
+            int row = board.getCenterRow();
+            int col = board.getCenterCol() - (finalMove.length()/2);
+
+            return constructMove(finalMove, row, col, true, true);
+        }
+        return null;
+    }
+
+    /**
+     * Creates a move given the available tiles in the rack and the current state of the board
+     * @param rack the list of tiles to use for the move
+     * @param board the board the move is played
+     * @param wordList the list of words to check if move is valid
+     * @return valid move to play
+     */
     public static Move generateSucceedingMove(TileRack rack, ScrabbleBoard board, WordList wordList){
         for (int row = 1; row < board.length(); row++) {
             for (int col = 1; col < board.length(); col++) {
@@ -42,39 +123,13 @@ public class MoveGenerator {
         return null;
     }
 
-    private static Move constructMove(String word, int row, int col, boolean goingRight, boolean firstMove){
-
-        StringBuilder sb = new StringBuilder();
-        String moveStr = firstMove ? word : word.substring(1);
-        sb.append(moveStr);
-        sb.append(',');
-        char rowLetter = (char) (col + Constants.CHAR_INT_VALUE_BEFORE_SMALL_LETTER_A);
-        sb.append(rowLetter);
-        sb.append(row);
-        sb.append(',');
-        char direction = goingRight ? 'r' : 'd';
-        sb.append(direction);
-
-        return new Move(sb.toString());
-    }
-
-    private static int downMoveAvailableSpace(ScrabbleBoard board, int row, int col){
-        int currRow = row;
-        int space = 0;
-        // Check: top of connecting square is not out of bounds and empty
-        if((currRow - 1) > 1 && !board.isSquareEmpty(currRow - 1, col)){
-            return 0;
-        }
-        // Check: available space downward
-        while(currRow < board.length()
-                && board.isSquareEmpty(currRow + 1, col)
-                && !board.isSquareHorizontalAdjacent(currRow + 1, col)){
-            space++;
-            currRow++;
-        }
-        return space;
-    }
-
+    /**
+     * Counts the number of available space for a move going right
+     * @param board the board the move is played
+     * @param row the starting row of the move
+     * @param col the starting column of the move
+     * @return number of available space going right
+     */
     private static int rightMoveAvailableSpace(ScrabbleBoard board, int row, int col){
         int currCol = col;
         int space = 0;
@@ -90,19 +145,5 @@ public class MoveGenerator {
             currCol++;
         }
         return space;
-    }
-
-    public static Move generateFirstMove(TileRack rack, ScrabbleBoard board, WordList wordList){
-        List<String> validMoves = WordGenerator.generateWords(rack.getTiles(), 7, "", wordList);
-        Optional<String> optionalMove = validMoves.stream().max(Comparator.comparing(String::length));
-        if(validMoves.size() > 0){
-            String finalMove = optionalMove.get();
-
-            int row = board.getCenterRow();
-            int col = board.getCenterCol() - (finalMove.length()/2);
-
-            return constructMove(finalMove, row, col, true, true);
-        }
-        return null;
     }
 }
