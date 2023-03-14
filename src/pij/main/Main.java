66f7d6e15d24e69f12b0338734b81cba57d9c468
nicolluisyumang <yumangnicol@gmail.com>
2023-03-14 22:29:57 +0000

    docs(Main): Add java docs to Main class

--- src/pij/main/Main.java
+++ src/pij/main/Main.java
@@ -1,10 +1,18 @@
 package pij.main;
 
+/**
+ * Represents a scrabble game java program
+ * @author Nicol Luis Yumang
+ */
 public class Main {
+
     public static void main(String[] args) {
         start();
     }
 
+    /**
+     * Creates a new scrabble game and starts it
+     */
     private static void start(){
         Game game = new Game();
         game.startGame();
