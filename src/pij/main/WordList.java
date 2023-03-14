893f5bffba0e4312ab97b04ff2c5885def7b99cc
nicolluisyumang <yumangnicol@gmail.com>
2023-03-14 19:01:59 +0000

    docs(WordList): Add java docs to WordList class

--- src/pij/main/WordList.java
+++ src/pij/main/WordList.java
@@ -3,9 +3,21 @@ package pij.main;
 import java.io.*;
 import java.util.HashSet;
 
+/**
+ * Represents the dictionary of valid words to play in a scrabble game
+ * @author Nicol Luis Yumang
+ */
 public class WordList {
+
+    /**
+     * The list of valid words to play
+     */
     private final HashSet<String> wordList;
 
+    /**
+     * Constructor: List of valid words
+     * @param fileName path to file
+     */
     public WordList(String fileName) {
         wordList = new HashSet<>();
         File file = new File(fileName);
@@ -21,6 +33,11 @@ public class WordList {
         }
     }
 
+    /**
+     * Check if the word is found in the list
+     * @param word string to check in word list
+     * @return true if found and false if not
+     */
     public  boolean contains(String word){
         return this.wordList.contains(word);
     }
