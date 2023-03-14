2f7f4fe68c3fbc7545fae12f12d5bdb181d0f243
nicolluisyumang <yumangnicol@gmail.com>
2023-03-14 19:04:08 +0000

    docs(Tile): Add java docs to Tile class

--- src/pij/main/Tile.java
+++ src/pij/main/Tile.java
@@ -1,26 +1,37 @@
 package pij.main;
 
 import pij.main.utils.TileSettings;
-
 import java.util.Objects;
 
+/**
+ * Represents a tile in a scrabble game
+ * @author Nicol Luis Yumang
+ */
 public class Tile {
+    /**
+     * The letter representation of a tile
+     */
     private final char letter;
+
+    /**
+     * The value of a tile
+     */
     private final int value;
 
+    /**
+     * Constructor: creates a tile from a given letter or wildcard
+     * The point value of a tile is set based on the TileSettings
+     * @param letter the letter character to create a tile
+     */
     public Tile(char letter) {
         this.letter = letter;
         this.value = Character.isLowerCase(letter) ? 3 : TileSettings.getLetterValue(letter);
     }
 
-    public char getLetter() {
-        return letter;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
+    /**
+     * Checks the equality of a tile with another object
+     * @return true if value object is equal to tile
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -29,6 +40,26 @@ public class Tile {
         return letter == tile.letter && value == tile.value;
     }
 
+    /**
+     * Gets the letter of a tile
+     * @return letter of tile
+     */
+    public char getLetter() {
+        return letter;
+    }
+
+    /**
+     * Gets the point value of a tile
+     * @return value of tile
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * Returns a hashcode for a tile object
+     * @return hashcode for tile object
+     */
     @Override
     public int hashCode() {
         return Objects.hash(letter, value);
